0225f09fa01d0bc9e7b85ff1b6d109482ed67dfa
My Name <cpgsom819@gmail.com>
2023-05-08 23:06:54 +0800

    adding category

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Home.css'
 // import {TextField }  from "@material-ui/core";
-import { TextField } from '@mui/material';
+import { MenuItem, TextField } from '@mui/material';
+import Categories from '../../Components/Data/Categories';
 const Home = () => {
   return (
     <div className='content'>
@@ -10,16 +11,40 @@ const Home = () => {
 
         <span style={{fontSize : 30}}>Quiz Settings </span>
 
-          <div className="settings_select">
+          <div className="settings__select">
           <TextField 
           style={{marginBottom : 25}}          
           label="Enter Your name " variant="outlined" />
 
+             
+        <TextField
+        
+        select
+        label="Select Category"
+        variant='outlined'
+        style={{marginBottom : 30}}  
+        > 
+
+          
+           {
+
+            Categories.map((cat) => (
+
+                        
+                  <MenuItem  key={cat.category} value={cat.value}>
+                    {cat.category}
+                  </MenuItem>
+
+            ))
+           }
+
+
+        
+
+        </TextField>
+
+
           </div>
-
-
-
-
       </div>
       <img src="/quiz.svg" className='banner' alt="" srcset="" />
 
