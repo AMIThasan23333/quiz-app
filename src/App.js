26993729dd0e256707bc5a719026dd218f024c63
My Name <cpgsom819@gmail.com>
2023-05-08 21:35:21 +0800

    restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './Components/Home/Home';
+import Home from './Pages/Home/Home';
+// import Home from './Components/Home/Home';
 
 function App() {
   return (
          
                 <BrowserRouter>
+
+                       <div>
                            
                            <Routes>
                            <Route path="/" element={<Home />} />
                            </Routes>
+
+                           </div>
                 
                 </BrowserRouter>
   );
---
+++ src/Components/ErrorMessage/ErrormMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrormMessage = () => {
+  return (
+    <div>ErrormMessage</div>
+  )
+}
+
+export default ErrormMessage
